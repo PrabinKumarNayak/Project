9241b56484d4165b14d782291be3f81ca0af2fe7
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-03-02 19:42:01 +0530

    react project 1

--- Project-1 REACT/project-1/src/App.jsx
+++ Project-1 REACT/project-1/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Hero from './components/hero';
 import Navigation from './components/navigation';
 function App() {
 
   return (
     <div>
       <Navigation/>
+      <Hero/>
     </div>
   )
 }
