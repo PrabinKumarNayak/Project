5ddc5ea077d88eea1769414888f9dd9cb6e6c322
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-04-06 10:43:11 +0530

    food app react

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Header from "../../components/Header/Header"
 import "./Home.css"
 export default function Home(){
     return(
         <div>
-            <h1>Home</h1>
+            <Header/>
         </div>
     )
 }
