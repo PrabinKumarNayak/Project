afa53de09b9ca32e83a29b319b68108b30807a78
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-04-25 19:14:27 +0530

    food app react

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import { useState } from "react"
 import Header from "../../components/Header/Header"
 import "./Home.css"
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay"
+
 export default function Home(){
     const[category,setCategory] = useState("All")
     return(
         <div>
             <Header/>
             <ExploreMenu setCategory = {setCategory} category = {category}/>
+            <FoodDisplay category = {category} />
         </div>
     )
 }
