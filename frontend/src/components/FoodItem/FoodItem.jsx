0c8da5b7b59bf09b6c7c774b2d257a361027fa73
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-04-27 20:39:20 +0530

    food app react

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
 import { assets } from "../../assets/Assets";
-
-export default function FoodItem({ id, name, price, description, image }){
-    return(
-        <div className="food-item">
-            <div className="food-item-image-container">
-                <img src={image} alt="" />
-            </div>
-            <div className="food-item-info">
-                <div className="food-item-name-rating">
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt="" />
-                </div>
-                <p className="food-item-desc" >{description}</p>
-                <p className="food-item-price">${price}</p>
-            </div>
+import "./FoodItem.css";
+export default function FoodItem({ _id, name, price, description, image }) {
+  const [itemCount, setItemCount] = useState(0);
+  return (
+    <div className="food-item">
+      <div className="food-item-image-container">
+        <img src={image} alt="" className="food-item-image" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
-    )
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
 }
