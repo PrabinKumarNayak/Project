bbf6c96182fc838583ae4bd8d5f0eca0d69ce150
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-09-12 11:02:46 +0530

    food app context setup

--- food-app/frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ food-app/frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,6 +1,6 @@
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/Assets";
-export default function ExploreMenu() {
+export default function ExploreMenu({category,setCategory}) {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore Our Menu</h1>
@@ -10,8 +10,8 @@ export default function ExploreMenu() {
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return(
-                <div key={index} className="explore-menu-list-item">
-                    <img src={item.menu_image} alt="" />
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                    <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
                     <p>{item.menu_name}</p>
                 </div>
             )
--- food-app/frontend/src/main.jsx
+++ food-app/frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StoreContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreContextProvider>
 );
