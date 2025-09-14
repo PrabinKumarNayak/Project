c51f90e87f187a2745dfd9b4a4b867d344de507d
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-09-14 22:54:31 +0530

    food app food-item functionality

--- food-app/frontend/src/components/FoodItem/FoodItem.jsx
+++ food-app/frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,19 +1,25 @@
-import { assets } from "../../assets/Assets"
-import "./FoodItem.css"
-export default function FoodItem({id,name,price,description,image}){
-    return(
-        <div className="food-item">
-            <div className="food-item-img-container">
-                <img src={image} alt="" className="food-item-image" />
-            </div>
-            <div className="food-item-info">
-                <div className="food-item-name-rating">
-                    <p>{name}</p>
-                    <img src={assets.rating_starts} alt="" />
-                </div>
-                <p className="food-item-desc">{description}</p>
-                <p className="food-item-price">${price}</p>
-            </div>
+import { useState } from "react";
+import { assets } from "../../assets/Assets";
+import "./FoodItem.css";
+export default function FoodItem({ id, name, price, description, image }) {
+  const [itemCount, setItemCount] = useState(0);
+  return (
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img src={image} alt="" className="food-item-image" />
+        {!itemCount
+            ?<img className="add" onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
+            : <div className="food-item-counter"></div>
+        }
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
         </div>
-    )
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
 }
