57185962bc89d5c41f4e9805fb086485684a9200
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-09-15 11:30:10 +0530

    food app food-item functionality at store context

--- food-app/frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ food-app/frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,24 +2,28 @@ import { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
-
-export default function FoodDisplay() {
+export default function FoodDisplay({ category }) {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you.</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              price={item.price}
-              description={item.description}
-              image={item.image}
-            />
-          );
+          if(category==="All" || category===item.category){
+            return (
+              <>
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  price={item.price}
+                  description={item.description}
+                  image={item.image}
+                />
+              </>
+            );
+          }
+          
         })}
       </div>
     </div>
