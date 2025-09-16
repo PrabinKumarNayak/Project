f1f15377b9c4704ca6ea76534d91aae62d4a350a
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-09-16 17:39:01 +0530

    footer

--- food-app/frontend/src/App.jsx
+++ food-app/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 
 export default function App(){
   return(
@@ -15,6 +16,7 @@ export default function App(){
         <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    <Footer/>
     </>
   )
 }
