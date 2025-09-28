b0639d7ae1bb7c44f6e33bb8bc843b80179f9757
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-09-28 21:48:16 +0530

    car rental website hero

--- CARRENTAL/client/src/App.jsx
+++ CARRENTAL/client/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./Pages/Home";
+import CartDetails from "./Pages/CartDetails";
+import Cars from "./Pages/Cars";
+import MyBookings from "./Pages/MyBookings";
 
-export default function App(){
-  return(
-    <div>
-      <Navbar/>
-    </div>
-  )
+export default function App() {
+  const { showLogin, setShowLogin } = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/car-details/:id" element={<CartDetails/>}/>
+        <Route path="/cars" element={<Cars/>}/>
+        <Route path="/my-bookings" element={<MyBookings/>}/>
+      </Routes>
+    </>
+  );
 }
---
+++ CARRENTAL/client/src/Pages/CartDetails.jsx
@@ -0,0 +1,7 @@
+export default function CartDetails(){
+    return(
+        <>
+        hi
+        </>
+    )
+}
