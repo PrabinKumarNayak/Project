306bd1b49220a5e7333d62350e1d7de02e323965
PrabinKumarNayak <nayakdilipkumar818@gmail.com>
2025-09-30 17:10:39 +0530

    car rental banner

--- CARRENTAL/client/src/components/CarCard.jsx
+++ CARRENTAL/client/src/components/CarCard.jsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom"
 import { assets } from "../assets/assets"
 
 export default function CarCard({car}){
     const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
     return(
-        <div className="group rounded-x1 overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer">
+        <div onClick={()=>{navigate(`/car-details/${car._id}`); scrollTo(0,0)}} className="group rounded-x1 overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer">
             <div className="relative h-48 overflow-hidden">
                 <img src={car.image} alt="Car Image" className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" />
 
